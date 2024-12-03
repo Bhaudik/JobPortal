214f2785bb88a33e20ff834c6d1831b0d5f18847
Bhaudik <pumbhadiyabhaudik@gmail.com>
2024-12-03 09:21:07 +0530

    create new recoed throw fectory and seeder

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    // Add any other model properties or methods here.
+}
